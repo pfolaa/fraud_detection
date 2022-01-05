3f1d53bbe1ca112f432273dabbe9176f948f8c75
pfolaa <92052457+pfolaa@users.noreply.github.com>
2022-01-05 22:01:55 +0100

    Add files via upload
    update file

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def predict():
 # Launch Flask application
 if __name__ == "__main__":
     #app.run(debug=True)
-    app.run(host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0", port=8080)
