0ee8f5905c96db5d0a62deae2e5f573f82cce9b3
pfolaa <92052457+pfolaa@users.noreply.github.com>
2022-02-01 21:30:18 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -93,8 +93,11 @@ def predict_from_json():
   print('outdir json: '+outdir_json)
   if not os.path.exists(outdir_json):
       os.makedirs(outdir_json, exist_ok=True)
-  with open(outdir_json) as jsonfile:
-    data = json.load(jsonfile)
+
+  with open(outdir_json, 'r') as jsonfile:
+    if len(jsonfile.readlines()) != 0:
+      jsonfile.seek(0)
+      data = json.load(jsonfile)
   #data = json.loads(outdir_json)
   print('data: '+data)
   df = pd.DataFrame.from_records(data)
